4db8e7e0ad76e1daca6d7f97d00c8c342fbaee35
Drew Hornbein <dhornbein@gmail.com>
2015-03-18 12:28:19 -0700

    added share template

---
+++ templates/share.php
@@ -0,0 +1,7 @@
+<!-- social share buttons -->
+<div class="entry-social pull-right">
+  <a href="#"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-envelope-o fa-stack-1x fa-inverse"></i></span></a>
+  <a href="#"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-facebook fa-stack-1x fa-inverse"></i></span></a>
+  <a href="#"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-twitter fa-stack-1x fa-inverse"></i></span></a>
+  <a href="#"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-pinterest fa-stack-1x fa-inverse"></i></span></a>
+</div>
